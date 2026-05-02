25e70cdf81c6d5277c914b239dd70ec50f74ff7e
Omar Jabour <jabour.omar@hotmail.com>
2026-05-03 00:18:43 +0200

    :closed_lock_with_key:  working on user schema ././   :closed_lock_with_key:

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -3,13 +3,13 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 @Schema()
 export class User extends Document {
     @Prop({ required: true })
-    name: string;
+    name!: string;
 
     @Prop({ required: true })
-    email: string;
+    email!: string;
 
     @Prop({ required: true })
-    password: string;
+    password!: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
