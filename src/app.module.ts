f9d0352cdcb4ae63ef3a02ff580133661d2d2fb3
Omar Jabour <jabour.omar@hotmail.com>
2026-05-03 00:53:47 +0200

    :closed_lock_with_key:  working on user dto  :closed_lock_with_key:

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule, AuthModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/nestjs-auth'), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
