58a60a3905216a89690b2ea8bf8471ed04ba8761
Omar Jabour <jabour.omar@hotmail.com>
2026-05-03 00:39:57 +0200

    :closed_lock_with_key:  working on user dto  .  :closed_lock_with_key:

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IsString, Matches, MinLength } from "class-validator";
+
 export class SignupDto {
+    @IsString()
     name!: string;
-    email!: string
+
+    @IsEmail()
+    email!: string;
+
+    @IsString()
+    @MinLength(6)
+    @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/, {
+        message: 'Password must be at least 6 characters long and contain at least one letter and one number',
+    })
     password!: string;
 }
