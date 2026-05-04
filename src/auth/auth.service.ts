8fb24652a199d2e4d514a9c56eda003ddb209d7a
Omar Jabour <jabour.omar@hotmail.com>
2026-05-04 19:55:58 +0200

    :closed_lock_with_key:  working on user dto /./.  :closed_lock_with_key:

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import type { SignupDto } from './dto/signup.dto';
-import { useContainer } from '../../node_modules/class-validator/esm2015/container';
+import { InjectModel } from '@nestjs/mongoose';
+import type { Model } from 'mongoose';
+import { User } from '../schemas/user.schema';
 
 
 @Injectable()
 export class AuthService {
+  constructor(@InjectModel(User.name) private UserModel: Model<User>) {}
   async signup(signupData: SignupDto) {
     //TODO: Check if email is in useContainer
     //TODO: Hash the password
