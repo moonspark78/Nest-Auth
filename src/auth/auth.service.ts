e0586dfc957eda6c343b30ced84ac93dc15af859
Omar Jabour <jabour.omar@hotmail.com>
2026-05-04 20:22:23 +0200

    :closed_lock_with_key:  working on user dto /  :closed_lock_with_key:

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import  { SignupDto } from './dto/signup.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import  { Model } from 'mongoose';
@@ -10,7 +10,12 @@ export class AuthService {
   constructor(@InjectModel(User.name) private UserModel: Model<User>) {}
   async signup(signupData: SignupDto) {
     //TODO: Check if email is in use
-    const emailInUse = await this.UserModel.findOne({ email: signupData.email });
+    const emailInUse = await this.UserModel.findOne({ 
+      email: signupData.email
+    });
+    if (emailInUse) {
+      throw new BadRequestException('Email is already in use');
+    }
     //TODO: Hash the password
     //TODO: Create the user in the database
   }
