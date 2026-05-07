661e8a706f578a1af0e46f51e284983912beac8d
Omar Jabour <jabour.omar@hotmail.com>
2026-05-07 19:49:05 +0200

    :closed_lock_with_key:  working on appModule ././././  :closed_lock_with_key:

--- src/schemas/refresh-token.schema.ts
+++ src/schemas/refresh-token.schema.ts
@@ -1,3 +1,8 @@
-import { Schema } from "@nestjs/mongoose";
+import { Prop, Schema } from "@nestjs/mongoose";
+import mongoose, {Document} from "mongoose";
 
 @Schema({versionKey: false, timestamps: true})
+export class RefreshToken extends Document {
+    @Prop({ required: true })
+    token: string;
+}
