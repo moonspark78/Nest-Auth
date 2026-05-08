3927e3fd2349177cb10ea715832323297caa0245
Omar Jabour <jabour.omar@hotmail.com>
2026-05-08 19:37:25 +0200

    :closed_lock_with_key:  working on appModule ././././  :closed_lock_with_key:

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,18 +4,24 @@ import { AuthController } from './auth.controller';
 import { User, UserSchema } from '../schemas/user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RefreshToken, RefreshTokenSchema } from '../schemas/refresh-token.schema';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [MongooseModule.forFeature([
-    {
-      name: User.name,
-      schema: UserSchema
-    },
-    {
-      name: RefreshToken.name,
-      schema: RefreshTokenSchema
-    }
-  ])],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+      {
+        name: RefreshToken.name,
+        schema: RefreshTokenSchema,
+      },
+    ]),
+
+    JwtModule,
+  ],
+
   controllers: [AuthController],
   providers: [AuthService],
 })
