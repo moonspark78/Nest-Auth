d48d13b49fac50d8b09b2dbaeaad2de28911d503
Omar Jabour <jabour.omar@hotmail.com>
2026-05-10 17:12:57 +0200

    :closed_lock_with_key:  working on authGuard ./.  :closed_lock_with_key:

--- src/guards/auth.guards.ts
+++ src/guards/auth.guards.ts
@@ -1,8 +1,10 @@
 import { Injectable, type CanActivate, type ExecutionContext } from "@nestjs/common";
 import type { Observable } from "rxjs";
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+    constructor(private JwtService: JwtService) {}
     canActivate(
         context: ExecutionContext,
     ): boolean | Promise<boolean> | Observable<boolean> {
