6cbeefb261464fd270ea1289f9da71b47e575cbc
Omar Jabour <jabour.omar@hotmail.com>
2026-05-10 17:10:23 +0200

    :closed_lock_with_key:  working on authGuard ././.  :closed_lock_with_key:

--- src/guards/auth.guards.ts
+++ src/guards/auth.guards.ts
@@ -6,6 +6,6 @@ export class AuthGuard implements CanActivate {
     canActivate(
         context: ExecutionContext,
     ): boolean | Promise<boolean> | Observable<boolean> {
-        
+        return true;
     }
 }
