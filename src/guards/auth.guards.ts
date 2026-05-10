e5cbd7a872df6f3389619ec9925b454309fd4ee0
Omar Jabour <jabour.omar@hotmail.com>
2026-05-10 17:20:37 +0200

    :closed_lock_with_key:  working on authGuard ./  :closed_lock_with_key:

--- src/guards/auth.guards.ts
+++ src/guards/auth.guards.ts
@@ -1,6 +1,7 @@
-import { Injectable, type CanActivate, type ExecutionContext } from "@nestjs/common";
-import type { Observable } from "rxjs";
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from "@nestjs/common";
+import { Observable } from "rxjs";
 import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -12,7 +13,12 @@ export class AuthGuard implements CanActivate {
         const token = this.extractTokenFromHeader(request);
 
         if (!token) {
-            return false;
+            throw new UnauthorizedException("No token provided");
+        }
+        try {
+            const payload = this.JwtService.verify(token);
+        } catch (e) {
+            throw new UnauthorizedException("Invalid token");
         }
     }
     private extractTokenFromHeader(request: Request): string | undefined {
