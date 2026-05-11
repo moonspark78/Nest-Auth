2af29c587239982e76ef930937c7d2f2dc1bf2d5
Omar Jabour <jabour.omar@hotmail.com>
2026-05-11 20:17:01 +0200

    :closed_lock_with_key:  working on authGuard /  :closed_lock_with_key:

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from './guards/auth.guards';
 
-@UseGuards()
+@UseGuards(AuthGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
